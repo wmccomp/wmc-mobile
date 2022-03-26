da5b229e57d229dd1ffa4d058dcb8721c5cc61f4
Emanuelle Neves <43800901+May199@users.noreply.github.com>
2022-03-26 02:37:00 -0300

    SplashAnimation e Navagation Initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './src/screens/Home';
+import SplashAnimation from './src/screens/SplashAnimation';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{headerShown: false}}>
+        
+        <Stack.Screen name="SplashAnimation" component={SplashAnimation} />
+        <Stack.Screen name="Home" component={Home} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
